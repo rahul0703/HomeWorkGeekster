a35a7d1d706c2589748b4e31a7ebc56759076b53
rahul007 <rahulsureka516@gmail.com>
2021-03-08 16:00:59 +0530

    stacks and queue last

--- src/StacksQueueHW/client.java
+++ src/StacksQueueHW/client.java
@@ -1,6 +1,8 @@
 package StacksQueueHW;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class client {
@@ -121,15 +123,186 @@ public class client {
         }
         return maxArea;
     }
-//    solve expression inflix......................................
 
-//    solve expression postflix...................................
+
+//    solve expression inflix......................................
+    public static int SolveInflixOperation(String str){
+        Stack<Integer> operands = new Stack<>();
+        Stack<Character> operator = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(Character.isDigit(ch)){
+                operands.push(ch - '0');
+            }else if(ch == '('){
+                operator.push(ch);
+            }else if(ch == ')'){
+                while(operator.peek() != '('){
+                    int opar2 = operands.pop();
+                    int opar1 = operands.pop();
+                    char oper = operator.pop();
+                    int ans = solveExpression(opar1, opar2, oper);
+                    operands.push(ans);
+                }
+                operator.pop();
+            }else if(operator.isEmpty()){
+                operator.push(ch);
+            }else if(ch == '/' || ch == '*' || ch == '+' || ch == '-'){
+                while(!operator.isEmpty() && operator.peek() != '(' && precdence(operator.peek()) >= precdence(ch)){
+                    int opar2 = operands.pop();
+                    int opar1 = operands.pop();
+                    char oper = operator.pop();
+                    int ans = solveExpression(opar1, opar2, oper);
+                    operands.push(ans);
+                }
+                operator.push(ch);
+            }
+        }
+        while (!operator.isEmpty()){
+            int opar2 = operands.pop();
+            int opar1 = operands.pop();
+            char oper = operator.pop();
+            int ans = solveExpression(opar1, opar2, oper);
+            operands.push(ans);
+        }
+        return operands.pop();
+    }
+
+    private static int solveExpression(int a, int b, char ch){
+        if(ch == '+')
+            return a + b;
+        else if(ch == '-')
+            return a - b;
+        else if(ch == '/')
+            return a / b;
+        else
+            return a * b;
+    }
+
+    private static int precdence(char ch){
+        if(ch == '+' || ch == '-'){
+            return 1;
+        }else if(ch == '/' || ch == '*'){
+            return 2;
+        }
+        return 0;
+    }
+////    solve expression postflix...................................
+
+    public static int solvePostFlixExpression(String str){
+        Stack<Integer> operands = new Stack<>();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(Character.isDigit(ch)){
+                operands.push(ch - '0');
+            }else if(ch == '+' || ch == '-' || ch == '/' || ch == '*'){
+                int b = operands.pop();
+                int a = operands.pop();
+                int ans = solveExpression(a, b, ch);
+                operands.push(ans);
+            }
+        }
+        return operands.pop();
+    }
+
+
 //    solve expression preflix.................................
+    public static int solvePreflixOperation(String str){
+        Stack<Integer> operands = new Stack<>();
+        for(int i = str.length()-1; i >= 0; i--){
+            char ch = str.charAt(i);
+            if(Character.isDigit(ch)){
+                operands.push(ch - '0');
+            }else if(ch == '-' || ch == '+' || ch == '/' || ch == '*'){
+                int oper1 = operands.pop();
+                int oper2 = operands.pop();
+                int ans = solveExpression(oper1, oper2, ch);
+                operands.push(ans);
+            }
+        }
+        return operands.pop();
+    }
+
+
 //    inflix to preflix.....................................
+
+
 //    postflix to inflix...................................
 //    inflix to postflix.......................................
 //    inflix to postflix.........................................
 //    Merge Intervals............................................
 //    celebrity Problem............................................
 
+/*
+    Home work questions.....................................................
+*/
+//    ======================================Question 1=========================================================================
+//Queue based approach for first non-repeating character in a stream
+    public static String[] firstNonRepeatingChar(String str){
+        Queue<Character> queue = new LinkedList<>();
+        int[] arrayCount = new int[27];
+        String[] answerArray = new String[str.length()];
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            arrayCount[ch - 'a']++;
+            if(queue.isEmpty()){
+                if(arrayCount[ch - 'a'] > 1){
+                    answerArray[i] = "-1";
+                }else{
+                    answerArray[i] = ch + "";
+                    queue.add(ch);
+                }
+            }else{
+                while(!queue.isEmpty() && arrayCount[queue.peek() - 'a'] > 1){
+                    queue.poll();
+                }
+                if(queue.isEmpty()){
+                    if(arrayCount[ch - 'a'] > 1){
+                        answerArray[i] = "-1";
+                    }else{
+                        answerArray[i] = ch + "";
+                        queue.add(ch);
+                    }
+                }else{
+                    answerArray[i] = queue.peek() + "";
+                    queue.add(ch);
+                }
+            }
+        }
+        return answerArray;
+    }
+
+
+//    ===========================================Question 2 last=========================================================
+//    Sort the stack using recursion...........................................................
+    public static void SortStack(Stack<Integer> stack){
+        sort(stack);
+        return;
+    }
+    private static void sort(Stack<Integer> stack){
+        if(stack.size() == 1 || stack.isEmpty()){
+            return;
+        }
+        int x = stack.pop();
+        sort(stack);
+        insert(stack, x);
+        return;
+    }
+
+    private static void insert(Stack<Integer> stack, int x){
+        if(stack.isEmpty()){
+            stack.push(x);
+            return;
+        }
+        if(stack.peek() < x){
+            int y = stack.pop();
+            insert(stack, x);
+            stack.push(y);
+        }else{
+            stack.push(x);
+        }
+        return;
+    }
+
+//    ==============================================Question 4last===================================================
+//
 }
--- src/StacksQueueHW/main.java
+++ src/StacksQueueHW/main.java
@@ -4,16 +4,29 @@ import java.util.*;
 public class main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int[] array = new int[n];
-        for(int i = 0; i < n; i++){
-            array[i] = scan.nextInt();
-        }
+//        int n = scan.nextInt();
+//        int[] array = new int[n];
+//        for(int i = 0; i < n; i++){
+//            array[i] = scan.nextInt();
+//        }
 //        int[] answer = client.NextGreater(array);
 //        int[] answer = client.previousGreaterElements(array);
 //        for(int i : answer){
 //            System.out.print(i + " ");
 //        }
-        System.out.println(client.largestHistogramArea(array));
+//        System.out.println(client.solvePreflixOperation("+9*26"));
+//        String[] answer = client.firstNonRepeatingChar("aabbccaabbcc");
+//        for(String s: answer){
+//            System.out.print(s + " ");
+//        }
+        Stack<Integer> stack = new Stack<>();
+        int[] array = {1, 3,2 ,5 ,4, 7, 4, 9, 5,7 ,4, 5,10, 12, 10, 21, 42, 32};
+        for(int i: array){
+            stack.push(i);
+        }
+        client.SortStack(stack);
+        while (!stack.isEmpty()){
+            System.out.print(stack.pop() + " ");
+        }
     }
 }
